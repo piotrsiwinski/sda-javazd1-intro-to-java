8cf5b8e452f79e6b3390ed693535aed6f3fea131
Piotr Siwinski <piotr.siwinski@f-secure.com>
2020-04-18 12:16:00 +0200

    zliczanie stringa

---
+++ src/petle/Zadanie3.java
@@ -0,0 +1,15 @@
+package petle;
+
+public class Zadanie3 {
+  // suma liczb od 0 - 100
+  public static void main(String[] args) {
+
+    int suma = 0;
+
+    for (int i = 1; i < 101; i++) {
+      suma = i;
+    }
+
+    System.out.println("Suma liczb od 1 do 100 to: " + suma);
+  }
+}
---
+++ src/petle/Zadanie4.java
@@ -0,0 +1,34 @@
+package petle;
+
+public class Zadanie4 {
+  public static void main(String[] args) {
+
+    // "Tomek";
+    // 0 -> T
+    // 1 -> o
+    // 2 -> m
+    // 3 -> e
+    // 4 -> k
+    // 0 - pierwsza  litera; ostani znak:  length() -1
+    String napis = "Ala ma kota, a kot ma Ale";
+    // tworzymy zmienna licznikowa do liczenia liter
+    int licznik = 0;
+    for (int i = 0; i < napis.length(); i++) {
+      char znak = napis.charAt(i);
+      System.out.println(znak);
+      // w kazdym obiegu petli zwieksz licznik
+      licznik++;
+    }
+
+    System.out.println("KONIEC PETLI FOR....");
+    System.out.println("dlugosc podanego napisu to: " + licznik);
+
+    // policzyc dlugosc napisu bez spacji
+
+    // implement task here
+    // zwiekszamy licznik, jezeli znak jest rozny od spacji
+
+    System.out.println("Dlugosc napisu bez spacji to: ");
+
+  }
+}
